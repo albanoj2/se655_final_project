71a4438c1d6da17c84efb37bd40a8446aeeb26da
Justin Albano <albano.justin@gmail.com>
2015-03-07 12:36:08 -0500

    Added individual Neo4j project

---
+++ individual/graph/individual_neo4j/src/main/java/edu/erau/se655/sp15/teama/final_project/individual_neo4j/App.java
@@ -0,0 +1,13 @@
+package edu.erau.se655.sp15.teama.final_project.individual_neo4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
